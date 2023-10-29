dcbd47849748feab0cb3b76db88a57366edaa5ba
Victor <vic.bra@hotmail.com>
2023-10-29 19:36:45 +0100

    Disabled endpoint

--- wp-content/plugins/AKDTU/endpoints/export-mailbox-label.php
+++ wp-content/plugins/AKDTU/endpoints/export-mailbox-label.php
@@ -15,25 +15,20 @@ function AKDTU_export_mailbox_label() {
 	## Check if we are on the correct page
 	if (strpos($_SERVER["REQUEST_URI"], 'export-mailbox-label.php') == 1) {
 
-		if (is_user_logged_in() && isset($_POST['user_name'])) {
-
-			require_once 'mpdf.php';
-
-			echo "a";
+		if (is_user_logged_in() && isset($_GET['user_name'])) {
+			require_once WP_PLUGIN_DIR . '/AKDTU/mpdf.php';
 
 			$user = wp_get_current_user();
 
-			$aptnum = (substr($user->user_login,0,4) == "lejl" ? substr($user->user_login,4,3) : "XXX");
+			$aptnum = (is_apartment_from_username($user->user_login) ? apartment_number_from_username($user->user_login) : "XXX");
 
 			$mpdf = new \AKDTUpdf([210, 297], 'landscape', 32, 10, 11, 11, 11);
 
-			$html = '<html style="margin:0; padding:0;"><body style="margin:0; padding:0;"><table style="border-top: 4px dashed lightgrey; border-bottom: 4px dashed lightgrey; font-family: \'calibri\'; height: 62px; width: 100%;"><tbody><tr><td style="width: 8.3cm"><div style="font-size: 24pt;">Kollegiebakken 19. Lejl:</div></td><td style="width: 1.5cm"><div style="font-size: 24pt;">' . $aptnum . '</div></td><td style="width: 17.8cm" align="right"><div style="font-size: 36pt; overflow: hidden;">' . $_POST['user_name'] . '</div></td></tr></tbody></table></body></html>';
+			$html = '<html style="margin:0; padding:0;"><body style="margin:0; padding:0;"><table style="border-top: 4px dashed lightgrey; border-bottom: 4px dashed lightgrey; font-family: \'calibri\'; height: 62px; width: 100%;"><tbody><tr><td style="width: 8.3cm"><div style="font-size: 24pt;">Kollegiebakken 19. Lejl:</div></td><td style="width: 1.5cm"><div style="font-size: 24pt;">' . $aptnum . '</div></td><td style="width: 17.8cm" align="right"><div style="font-size: 36pt; overflow: hidden;">' . $_GET['user_name'] . '</div></td></tr></tbody></table></body></html>';
 
 			$mpdf->WriteHTML($html);
 
 			$mpdf->Output('AKDTU - Mailbox label - apt. ' . $aptnum . '.pdf', 'd');
-
-			echo "file made";
 
 		} else {
 			$protocol = isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0';
--- wp-content/plugins/AKDTU/register_endpoints.php
+++ wp-content/plugins/AKDTU/register_endpoints.php
@@ -1,6 +1,9 @@
 <?php
 
+// Export common house calendar as .ics
 include_once "endpoints/export-calendar.php";
-include_once "endpoints/export-mailbox-label.php";
+
+// Export pdf with label for letter-box. Disabled
+// include_once "endpoints/export-mailbox-label.php";
 
 ?>
