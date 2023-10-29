bc8c1dfec608fbf971242f82c090af7f94e6f922
Victor <vic.bra@hotmail.com>
2023-10-29 19:10:32 +0100

    Moved things into seperate files

---
+++ wp-content/plugins/AKDTU/register_functions.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "functions/bestyrelsesdokumenter.php";
+require_once "functions/delete_users.php";
+require_once "functions/display_apartments.php";
+require_once "functions/fælleshus.php";
+require_once "functions/notice.php";
+require_once "functions/send_mail.php";
+require_once "functions/users.php";
+require_once "functions/vlan.php";
+
+?>
