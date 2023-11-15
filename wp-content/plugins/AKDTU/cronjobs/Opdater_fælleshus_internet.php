a7b750e2f8041eee9a8a21a64c21c34cbe2a83cf
Victor <vic.bra@hotmail.com>
2023-11-15 18:29:47 +0100

    Bug fix and cleanup

--- wp-content/plugins/AKDTU/cronjobs/Opdater_fælleshus_internet.php
+++ wp-content/plugins/AKDTU/cronjobs/Opdater_fælleshus_internet.php
@@ -8,8 +8,9 @@
  * Cron-job responsible for sending information about the changing of the password to the router in the common house
  * 
  * @param bool $debug Flag, for whether the users should actually be deleted (false), or if this is a test run to show sample results (true)
+ * @param bool $force_send Flag, for whether to force sending emails with information to the Board and renter (if renter exists)
  */
-function send_opdater_fælleshus_internet($debug = false) {
+function send_opdater_fælleshus_internet($debug = false, $force_send = false) {
 	# Cron-job frequency
 	$run_every_hours_amount = 24; # How many hours go between each run of the cron-job
 
@@ -30,9 +31,9 @@ function send_opdater_fælleshus_internet($debug = false) {
 
 	# Extract values from struct
 	$new_password = $password_struct['password'];
-	$password_should_be_changed = $password_struct['should_be_changed'];
+	$password_should_be_changed = $password_struct['should_be_changed'] || $force_send;
 	$event = $password_struct['event'];
-	$send_mail_to_renter = $password_struct['send_mail_to_renter'];
+	$send_mail_to_renter = $password_struct['send_mail_to_renter'] || $force_send;
 	$rented_state = $password_struct['rented_state'];
 
 	# Check if this is a real run, and the password should be changed
@@ -56,8 +57,8 @@ function send_opdater_fælleshus_internet($debug = false) {
 		);
 	}
 
-	# Check if the password should be changed
-	if ($debug || $password_should_be_changed) {
+	# Check if an email should be sent to the Board or echoed
+	if ($debug || ($password_should_be_changed && FÆLLESHUS_INTERNET_TO != '')) {
 		# Replacements for email subject
 		$subject_replaces = array();
 
@@ -73,74 +74,71 @@ function send_opdater_fælleshus_internet($debug = false) {
 			echo '<h3>Mail til bestyrelsen:</h3>';
 		}
 		
-		# Check if an email should be sent or echoed
-		if (FÆLLESHUS_INTERNET_TO != '' || $debug) {
+		# Send or echo email
+		send_AKDTU_email($debug, $subject_replaces, $content_replaces, 'FÆLLESHUS_INTERNET');
+	}
+
+	# Check if an email should be sent to a renter or echoed
+	if ($debug || ($password_should_be_changed && $send_mail_to_renter)) {
+		# Get the language of the event (Assumed the same as the language of the renter)
+		$lang = pll_get_post_language($event->post_id);
+
+		# Check if an email in Danish should be sent or echoed
+		if ($debug || ($lang == "da" && FÆLLESHUS_INTERNET_BRUGER_DA_TOGGLE)) {
+			# Get owner of the event
+			$event_owner = get_user_by('ID', $event->owner);
+
+			# Replacements for email subject
+			$subject_replaces_da = array();
+
+			# Replacements for email content
+			$content_replaces_da = array(
+				'#SSID' => $status["ssid"],
+				'#NEWPASS' => $status["password"],
+				'#RENTALSTARTDATE' => $date_da->format(new DateTime($event->event_start_date . " " . $event->event_start_time, new DateTimeZone('Europe/Copenhagen'))),
+				'#RENTALENDDATE' => $date_da->format(new DateTime($event->event_end_date . " " . $event->event_end_time, new DateTimeZone('Europe/Copenhagen'))),
+				'#RENTALSTARTTIME' => $time_da->format(new DateTime($event->event_start_date . " " . $event->event_start_time, new DateTimeZone('Europe/Copenhagen'))),
+				'#RENTALENDTIME' => $time_da->format(new DateTime($event->event_end_date . " " . $event->event_end_time, new DateTimeZone('Europe/Copenhagen'))),
+				'#FIRSTNAME' => $event_owner->first_name,
+				'#LASTNAME' => $event_owner->last_name,
+			);
+
+			# Write header if this is a test-run
+			if ($debug) {
+				echo '<hr><h3>Mail til beboer, dansk: sendes' . (FÆLLESHUS_INTERNET_BRUGER_DA_TOGGLE ? "" : " ikke") . '</h3>';
+			}
+
 			# Send or echo email
-			send_AKDTU_email($debug, $subject_replaces, $content_replaces, 'FÆLLESHUS_INTERNET');
+			send_AKDTU_email($debug, $subject_replaces_da, $content_replaces_da, 'FÆLLESHUS_INTERNET_BRUGER_DA', $event_owner->user_email);
 		}
 
-		# Check if an email should be sent to a renter
-		if ($debug || $send_mail_to_renter) {
-			# Get the language of the event (Assumed the same as the language of the renter)
-			$lang = pll_get_post_language($event->post_id);
+		# Check if an email in English should be sent or echoed
+		if ($debug || ($lang == "en" && FÆLLESHUS_INTERNET_BRUGER_EN_TOGGLE)) {
+			# Get owner of the event
+			$event_owner = get_user_by('ID', $event->owner);
+			
+			# Replacements for email subject
+			$subject_replaces_en = array();
 
-			# Check if an email in Danish should be sent or echoed
-			if ($debug || ($lang == "da" && FÆLLESHUS_INTERNET_BRUGER_DA_TOGGLE)) {
-				# Write header if this is a test-run
-				if ($debug) {
-					echo '<hr><h3>Mail til beboer, dansk: sendes' . (FÆLLESHUS_INTERNET_BRUGER_DA_TOGGLE ? "" : " ikke") . '</h3>';
-				}
+			# Replacements for email content
+			$content_replaces_en = array(
+				'#SSID' => $status["ssid"],
+				'#NEWPASS' => $status["password"],
+				'#RENTALSTARTDATE' => $date_en->format(new DateTime($event->event_start_date . " " . $event->event_start_time, new DateTimeZone('Europe/Copenhagen'))),
+				'#RENTALENDDATE' => $date_en->format(new DateTime($event->event_end_date . " " . $event->event_end_time, new DateTimeZone('Europe/Copenhagen'))),
+				'#RENTALSTARTTIME' => $time_en->format(new DateTime($event->event_start_date . " " . $event->event_start_time, new DateTimeZone('Europe/Copenhagen'))),
+				'#RENTALENDTIME' => $time_en->format(new DateTime($event->event_end_date . " " . $event->event_end_time, new DateTimeZone('Europe/Copenhagen'))),
+				'#FIRSTNAME' => $event_owner->first_name,
+				'#LASTNAME' => $event_owner->last_name,
+			);
 
-				# Get owner of the event
-				$event_owner = get_user_by('ID', $event->owner);
-
-				# Replacements for email subject
-				$subject_replaces_da = array();
-
-				# Replacements for email content
-				$content_replaces_da = array(
-					'#SSID' => $status["ssid"],
-					'#NEWPASS' => $status["password"],
-					'#RENTALSTARTDATE' => $date_da->format(new DateTime($event->event_start_date . " " . $event->event_start_time, new DateTimeZone('Europe/Copenhagen'))),
-					'#RENTALENDDATE' => $date_da->format(new DateTime($event->event_end_date . " " . $event->event_end_time, new DateTimeZone('Europe/Copenhagen'))),
-					'#RENTALSTARTTIME' => $time_da->format(new DateTime($event->event_start_date . " " . $event->event_start_time, new DateTimeZone('Europe/Copenhagen'))),
-					'#RENTALENDTIME' => $time_da->format(new DateTime($event->event_end_date . " " . $event->event_end_time, new DateTimeZone('Europe/Copenhagen'))),
-					'#FIRSTNAME' => $event_owner->first_name,
-					'#LASTNAME' => $event_owner->last_name,
-				);
-
-				# Send or echo email
-				send_AKDTU_email($debug, $subject_replaces_da, $content_replaces_da, 'FÆLLESHUS_INTERNET_BRUGER_DA', $event_owner->user_email);
+			# Write header if this is a test-run
+			if ($debug) {
+				echo '<hr><h3>Mail til beboer, engelsk: sendes' . (FÆLLESHUS_INTERNET_BRUGER_EN_TOGGLE ? "" : " ikke") . '</h3>';
 			}
 
-			# Check if an email in English should be sent or echoed
-			if ($debug || ($lang == "en" && FÆLLESHUS_INTERNET_BRUGER_EN_TOGGLE)) {
-				# Write header if this is a test-run
-				if ($debug) {
-					echo '<hr><h3>Mail til beboer, engelsk: sendes' . (FÆLLESHUS_INTERNET_BRUGER_EN_TOGGLE ? "" : " ikke") . '</h3>';
-				}
-
-				# Get owner of the event
-				$event_owner = get_user_by('ID', $event->owner);
-				
-				# Replacements for email subject
-				$subject_replaces_en = array();
-
-				# Replacements for email content
-				$content_replaces_en = array(
-					'#SSID' => $status["ssid"],
-					'#NEWPASS' => $status["password"],
-					'#RENTALSTARTDATE' => $date_en->format(new DateTime($event->event_start_date . " " . $event->event_start_time, new DateTimeZone('Europe/Copenhagen'))),
-					'#RENTALENDDATE' => $date_en->format(new DateTime($event->event_end_date . " " . $event->event_end_time, new DateTimeZone('Europe/Copenhagen'))),
-					'#RENTALSTARTTIME' => $time_en->format(new DateTime($event->event_start_date . " " . $event->event_start_time, new DateTimeZone('Europe/Copenhagen'))),
-					'#RENTALENDTIME' => $time_en->format(new DateTime($event->event_end_date . " " . $event->event_end_time, new DateTimeZone('Europe/Copenhagen'))),
-					'#FIRSTNAME' => $event_owner->first_name,
-					'#LASTNAME' => $event_owner->last_name,
-				);
-
-				# Send or echo email
-				send_AKDTU_email($debug, $subject_replaces_en, $content_replaces_en, 'FÆLLESHUS_INTERNET_BRUGER_EN', $event_owner->user_email);
-			}
+			# Send or echo email
+			send_AKDTU_email($debug, $subject_replaces_en, $content_replaces_en, 'FÆLLESHUS_INTERNET_BRUGER_EN', $event_owner->user_email);
 		}
 	}
 }
