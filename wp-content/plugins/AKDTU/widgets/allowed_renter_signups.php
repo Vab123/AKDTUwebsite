9fe259bf1f3c0d1291a2f728fcec86f1a8c992ab
Victor <vic.bra@hotmail.com>
2023-11-15 20:08:51 +0100

    Removed two columns

--- wp-content/plugins/AKDTU/widgets/allowed_renter_signups.php
+++ wp-content/plugins/AKDTU/widgets/allowed_renter_signups.php
@@ -15,10 +15,8 @@ function allowed_renter_signups_widget() {
 		<table id='dbem-bookings-table' class='widefat post ' style="max-width:75em">
 			<colgroup>
 				<col span="1" style="width: 10%" />
-				<col span="1" style="width: 15%" />
-				<col span="1" style="width: 30%" />
-				<col span="1" style="width: 15%" />
-				<col span="1" style="width: 15%" />
+				<col span="1" style="width: 35%" />
+				<col span="1" style="width: 40%" />
 				<col span="1" style="width: 15%" />
 			</colgroup>
 			<thead>
@@ -26,8 +24,6 @@ function allowed_renter_signups_widget() {
 					<th class='manage-column' scope='col'>Lejl.</th>
 					<th class='manage-column' scope='col'>Telefon</th>
 					<th class='manage-column' scope='col'>Periode</th>
-					<th class='manage-column' scope='col'>Klargjort</th>
-					<th class='manage-column' scope='col'>Overtaget</th>
 					<th class='manage-column' scope='col'>Handlinger</th>
 				</tr>
 			</thead>
@@ -44,8 +40,6 @@ function allowed_renter_signups_widget() {
 						<td style="vertical-align:middle"><?php echo $user->apartment_number; ?></td>
 						<td style="vertical-align:middle"><?php echo $user->phone_number; ?></td>
 						<td style="vertical-align:middle"><?php echo (new DateTime($user->start_time))->format('d-m-Y H:i') . ' - ' . (new DateTime($user->end_time))->format('d-m-Y H:i'); ?></td>
-						<td style="vertical-align:middle"><?php echo ($user->initial_reset == 1 ? "Ja" : "Nej"); ?></td>
-						<td style="vertical-align:middle"><?php echo ($user->initial_takeover == 1 ? "Ja" : "Nej"); ?></td>
 						<td style="vertical-align:middle;text-align:center">
 							<form action="" method="post">
 								<input type="hidden" name="user" value="<?php echo $user->apartment_number; ?>" />
