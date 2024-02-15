333b6a456162421c9b1d59c5c6dc8fdef1002b96
Victor Brandsen <vic.bra@hotmail.com>
2024-02-15 12:14:54 +0100

    Fixed bug with recognising akdtu email adresses

--- wp-content/themes/neve-child/functions.php
+++ wp-content/themes/neve-child/functions.php
@@ -189,14 +189,14 @@ pll_register_string('simple-membership', 'RENTAL_AFTER_APARTMENTNUM_APPROVED', '
 ## Dont send email change notifications when changing to or from @akdtu.dk
 if (!function_exists('send_email_change_email')) {
 	function send_email_change_email($return, $user, $userdata) {
-		return $return && strtolower(explode("@", $userdata['user_email'])[1]) != '@akdtu.dk' && strtolower(explode("@", $user['user_email'])[1]) != '@akdtu.dk';
+		return $return && strtolower(explode("@", $userdata['user_email'])[1]) != 'akdtu.dk' && strtolower(explode("@", $user['user_email'])[1]) != 'akdtu.dk';
 	}
 	add_filter('send_email_change_email', 'send_email_change_email', 10, 3);
 }
 ## Dont send password change notifications when changing to or from @akdtu.dk
 if (!function_exists('send_password_change_email')) {
 	function send_password_change_email($return, $user, $userdata) {
-		return $return && strtolower(explode("@", $userdata['user_email'])[1]) != '@akdtu.dk' && strtolower(explode("@", $user['user_email'])[1]) != '@akdtu.dk';
+		return $return && strtolower(explode("@", $userdata['user_email'])[1]) != 'akdtu.dk' && strtolower(explode("@", $user['user_email'])[1]) != 'akdtu.dk';
 	}
 	add_filter('send_password_change_email', 'send_password_change_email', 10, 3);
 }
