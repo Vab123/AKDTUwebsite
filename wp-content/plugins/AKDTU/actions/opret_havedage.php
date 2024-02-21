349f7454e8210fc01d201af018f548123a468839
Victor Brandsen <vic.bra@hotmail.com>
2024-02-21 14:33:46 +0100

    Adding new events to menu automatically

--- wp-content/plugins/AKDTU/actions/opret_havedage.php
+++ wp-content/plugins/AKDTU/actions/opret_havedage.php
@@ -44,6 +44,8 @@ function opret_havedage($danish_name, $danish_post_content, $english_name, $engl
 	$danish_args = array();
 	$english_args = array();
 
+	$rsvp = true;
+
 	# Go through all planned gardendays
 	foreach($gardendays as $date){
 		# Add a structure to list of Danish garden days
@@ -53,7 +55,7 @@ function opret_havedage($danish_name, $danish_post_content, $english_name, $engl
 			'lang' => 'da', # Language of the post
 			'date' => $date, # Day of the garden day
 			'rsvp' => array(
-				'rsvp' => $date == max($gardendays), # If signups are enabled. Only on the last gardenday
+				'rsvp' => $rsvp, # If signups are enabled. Only on the last gardenday
 				'date' => $latest_signup->format('Y-m-d'), # Date of last day to sign up
 				'time' => $latest_signup->format('H:i:s'), # Time of last day to sign up
 				'spaces' => $spaces, # Amount of spaces on this specific garden day
@@ -70,7 +72,7 @@ function opret_havedage($danish_name, $danish_post_content, $english_name, $engl
 			'lang' => 'en', # Language of the post
 			'date' => $date, # Day of the garden day
 			'rsvp' => array(
-				'rsvp' => $date == max($gardendays), # If signups are enabled. Only on the last gardenday
+				'rsvp' => $rsvp, # If signups are enabled. Only on the last gardenday
 				'date' => $latest_signup->format('Y-m-d'), # Date of last day to sign up
 				'time' => $latest_signup->format('H:i:s'), # Time of last day to sign up
 				'spaces' => $spaces, # Amount of spaces on this specific garden day
@@ -79,6 +81,8 @@ function opret_havedage($danish_name, $danish_post_content, $english_name, $engl
 			'event_lang' => 'en_US', # Language of the event
 			'publish_date' => $publish_date # Date from where signup is allowed
 		));
+
+		$rsvp = false;
 	}
 
 	# Create garden days
@@ -88,21 +92,83 @@ function opret_havedage($danish_name, $danish_post_content, $english_name, $engl
 	# Save the fact that corresponding events are translations of eachother
 	for ($i = 0; $i < count($danish_events); $i++){
 		pll_save_post_translations(array(
-			'da' => $danish_events[$i]['id'],
-			'en' => $english_events[$i]['id']
+			'da' => $danish_events[$i]['post_id'],
+			'en' => $english_events[$i]['post_id']
 		));
 	}
 
-	/**
-	 * @todo Find a way to automate this
-	 */
-	# Success. Write success message to admin interface, with info about what to add to the website menu.
-	$notice = new AKDTU_notice('success','Danske havedage oprettet. Tilføj begivenhed med slug ' . $danish_events[0]['slug'] . ' til menu og ændr eventuelt offentliggørelsesdato.');
-	$notice->render();
+	$date_da = new IntlDateFormatter('da_DK', IntlDateFormatter::SHORT, IntlDateFormatter::SHORT);
+	$date_da->setPattern('dd. MMMM YYYY HH:mm');
+	$publish_date_as_string = $date_da->format(new DateTime($publish_date));
 
-	# Success. Write success message to admin interface, with info about what to add to the website menu.
-	$notice = new AKDTU_notice('success','Engelske havedage oprettet. Tilføj begivenhed med slug ' . $english_events[0]['slug'] . ' til menu og ændr eventuelt offentliggørelsesdato.');
-	$notice->render();
+	// Add first Danish event to the correct menu
+	$danish_menu = wp_get_nav_menu_object( 'Dansk - logget ind' );
+	$danish_menu_root_event_id = 3455;	// ID of "Havedage" page
+	if(add_event_to_menu($danish_menu, $danish_menu_root_event_id, $danish_events[0])) {
+		new AKDTU_notice('success', "Danske havedage blev oprettet og automatisk tilføjet hjemmesidens menu for beboere. Begivenhederne bliver automatisk offentliggjort " . $publish_date_as_string);
+	}
+	else {
+		new AKDTU_notice('error', "Danish events could not be added to menu automatically. Slug is " . $danish_events[0]['event_slug']);
+	}
+	
+	// Add first English event to the correct menu
+	$english_menu = wp_get_nav_menu_object( 'Engelsk - logget ind' );
+	$english_menu_root_event_id = 3499;	// ID of "Garden days" page
+	if(add_event_to_menu($english_menu, $english_menu_root_event_id, $english_events[0])) {
+		new AKDTU_notice('success', "Engelske havedage blev oprettet og automatisk tilføjet hjemmesidens menu for beboere. Begivenhederne bliver automatisk offentliggjort " . $publish_date_as_string);
+	}
+	else {
+		new AKDTU_notice('error', "English events could not be added to menu automatically. Slug is " . $english_events[0]['event_slug']);
+	}
+}
+
+/**
+ * Programmatically add a new menu item
+ * 
+ * @param WP_Term $menu The menu to add a new item to. Can be created by calling wp_get_nav_menu_object($menu_name).
+ * @param int $menu_root_event_id ID of the event that will be the immediate parent of the new menu item.
+ * @param array $event_info Dictionary of event data. Must contain the following keys: post_id, post_title, event_slug
+ * 
+ * @return bool True if the event was added to the menu successfully
+ */
+function add_event_to_menu($menu, $menu_root_event_id, $event_info) {
+	if ( is_nav_menu( $menu ) ) {
+		$menu_item_data = array(
+			'menu-item-db-id' => 0,								// ID of the menu item. 0 means create new item.
+			'menu-item-object' => 'event',						// Type of element, the new menu item points to.
+			'menu-item-object-id' => $event_info['post_id'],	// ID of the post, this menu item is for.
+			'menu-item-parent-id' => $menu_root_event_id,		// ID of menu item, that is going to be the parent of the new item.
+			'menu-item-position' => '',							// Position of the post. Blank means at the end.
+			'menu-item-type' => 'post_type',					// Type of menu item. Same as regular posts.
+			'menu-item-title' => $event_info['post_title'],		// Title of the menu item. Set to the same as the event/post.
+			'menu-item-url' => $event_info['event_slug'],		// Slug of the event.
+			'menu-item-description' => '',						// Description of the menu item. Only used if the wordpress theme supports it.
+			'menu-item-attr-title' => '',						// Title attribute. Only used if the wordpress theme supports it.
+			'menu-item-target' => '',							// Set to '_blank' to open link in a new tab. Set to '' to open link in current tab.
+			'menu-item-classes' => '',							// Additional html classes to add to the menu item.
+			'menu-item-xfn' => '',								// Unknown.
+			'menu-item-status' => 'publish' 					// Immediately publish new menu item (NOT post or event).
+		);
+
+		// Create menu item
+		$error = wp_update_nav_menu_item(
+			$menu->term_id,		// ID of menu to edit.
+			0,					// ID of the menu item. 0 means create new item.
+			$menu_item_data,	// Menu item data. Created above.
+			true				// Do fire after insert hooks.
+		);
+
+		// Check if the menu item was created successfully
+		if (is_int($error)) {
+			// Success
+			return true;
+		}
+		else {
+			// Failure
+			new AKDTU_notice('error', $error->get_error_message());
+			return false;
+		}
+	}
 }
 
 /**
@@ -131,10 +197,10 @@ function opret_havedag($args){
 
 		# Check if the event should be published or set to pending
 		# The event with signups enabled are always published, so it can be added to menu and hidden after. Other events are published when they are set to be published.
-		$is_pending_status = ($rsvp['rsvp'] ? 1 : (strtotime($publish_date) > strtotime('now') ? 0 : 1));
+		$is_pending_status = (strtotime($publish_date) > strtotime('now') ? 0 : 1);
 
 		# Create Wordpress post
-		$id = wp_insert_post(array('post_status' => ($is_pending_status ? 'publish' : 'pending'), 'post_type' => 'event', 'post_title' => $title,'post_content' => $post_content, 'post_date' => ($rsvp['rsvp'] ? current_time("Y-m-d H:i:s") : $publish_date)));
+		$id = wp_insert_post(array('post_status' => ($is_pending_status ? 'publish' : 'pending'), 'post_type' => 'event', 'post_title' => $title,'post_content' => $post_content, 'post_date' => $publish_date));
 
 		# Protect event
         SwpmProtection::get_instance()->apply(array($id), 'custom_post')->save();
@@ -212,7 +278,12 @@ function opret_havedag($args){
 		}
 
 		# Add array with info to return object
-		$events_info[] = array('id'=>$id,'slug'=>$event->event_slug);
+		$events_info[] = array(
+			'post_id' => $id,
+			'event_id' => $event->event_id,
+			'event_slug' => $event->event_slug,
+			'post_title' => $title
+		);
 	}
 
 	# Return info about all created garden days
--- wp-content/plugins/AKDTU/widgets/opret_havedage.php
+++ wp-content/plugins/AKDTU/widgets/opret_havedage.php
@@ -16,41 +16,55 @@ function opret_havedag_dashboard_widget() { ?>
 			<tbody>
 				<tr>
 					<td><label>Dansk navn</label></td>
-					<td><input type="text" name="danish_name" placeholder="Havedag - Efterår 2022" /></td>
+					<td><input type="text" name="danish_name" value="Havedag - <?php echo (intval(date('n')) >= 4 && intval(date('n')) <= 10 ? "Efterår " . date('Y') : "Forår " . (intval(date('n')) < 4 ? date('Y') : strval(intval(date('Y') + 1)))); ?>" style="width:100%;" /></td>
 				</tr>
 				<tr>
 					<td><label>Dansk tekst</label></td>
-					<td><textarea cols="30" rows="4" name="danish_post_content">Tilmeld dig kun havedag én dag. Man er velkommen til at deltage flere dage end den man har tilmeldt sig, men i så fald bedes man sende en besked til bestyrelsen på <a href="mailto:bestyrelsen@akdtu.dk">bestyrelsen@akdtu.dk</a>.
-Du kan ændre hvilken dato du har tilmeldt dig senere, indtil midnat d. 18/09/2022 eller dagene er fyldt. Det gør du ved først at afmelde dig din nuværende dag <a href="/for-beboere/havedage/mine-tilmeldinger/">her</a>, og derefter tilmelde dig en anden dag i stedet.</textarea></td>
+					<td><textarea rows="35" name="danish_post_content" style="width:100%;">Tilmeld dig kun havedag én dag. Man er velkommen til at deltage flere dage end den man har tilmeldt sig, men i så fald bedes man sende en besked til bestyrelsen på <a href="mailto:bestyrelsen@akdtu.dk">bestyrelsen@akdtu.dk</a>.
+Du kan ændre hvilken dato du har tilmeldt dig senere, indtil midnat d. <?php echo "01" . "/" . (intval(date('n')) >= 4 && intval(date('n')) <= 10 ? "10" : "04") . "/" . (intval(date('n')) < 4 ? date('Y') : strval(intval(date('Y') + 1))); ?> eller dagene er fyldt. Det gør du ved først at afmelde dig din nuværende dag <a href="/for-beboere/havedage/mine-tilmeldinger/">her</a>, og derefter tilmelde dig en anden dag i stedet.
+
+I foråret bliver havedagenes overordnede emner følgende:
+- 1. <?php echo (intval(date('n')) >= 4 && intval(date('n')) <= 10 ? "oktober" : "april"); ?>: EMNE
+- 2. <?php echo (intval(date('n')) >= 4 && intval(date('n')) <= 10 ? "oktober" : "april"); ?>: EMNE
+- 3. <?php echo (intval(date('n')) >= 4 && intval(date('n')) <= 10 ? "oktober" : "april"); ?>: EMNE
+
+Hvis du har nogen spørgsmål eller kommentarer er du velkommen til at kontakte bestyrelsen på <a href="mailto:bestyrelsen@akdtu.dk">bestyrelsen@akdtu.dk</a>.</textarea></td>
 				</tr>
 				<tr>
 					<td><label>Engelsk navn</label></td>
-					<td><input type="text" name="english_name" placeholder="Gardenday - Fall 2022" /></td>
+					<td><input type="text" name="english_name" value="Gardenday - <?php echo (intval(date('n')) >= 4 && intval(date('n')) <= 10 ? "Fall " . date('Y') : "Spring " . (intval(date('n')) < 4 ? date('Y') : strval(intval(date('Y') + 1)))); ?>" style="width:100%;" /></td>
 				</tr>
 				<tr>
 					<td><label>Engelsk tekst</label></td>
-					<td><textarea cols="30" rows="4" name="english_post_content">Only sign up for one garden day. You are welcome to join other days as well, but in that case please send a message to the board at <a href="mailto:bestyrelsen@akdtu.dk">bestyrelsen@akdtu.dk</a>.
-You can change which date you want to attend later, until 18/09/2022 at 12 am. or the dates are full. This is done by cancelling your current selection <a href="/en/for-residents/garden-days/my-signups/">here</a>, and afterwards selecting a different day instead.</textarea></td>
+					<td><textarea rows="35" name="english_post_content" style="width:100%;">Only sign up for one garden day. You are welcome to join other days as well, but in that case please send a message to the board at <a href="mailto:bestyrelsen@akdtu.dk">bestyrelsen@akdtu.dk</a>.
+You can change which date you want to attend later, until <?php echo "01" . "/" . (intval(date('n')) >= 4 && intval(date('n')) <= 10 ? "10" : "04") . "/" . (intval(date('n')) < 4 ? date('Y') : strval(intval(date('Y') + 1))); ?> at 12 am. or the dates are full. This is done by cancelling your current selection <a href="/en/for-residents/garden-days/my-signups/">here</a>, and afterwards selecting a different day instead.
+
+In the spring, the garden days will have the following overall topics:
+- <?php echo (intval(date('n')) >= 4 && intval(date('n')) <= 10 ? "October" : "April"); ?> 1st: EMNE
+- <?php echo (intval(date('n')) >= 4 && intval(date('n')) <= 10 ? "October" : "April"); ?> 2nd: EMNE
+- <?php echo (intval(date('n')) >= 4 && intval(date('n')) <= 10 ? "October" : "April"); ?> 3rd: EMNE
+
+If you have any questions or comments, you are welcome to contact the Board on <a href="mailto:bestyrelsen@akdtu.dk">bestyrelsen@akdtu.dk</a>.</textarea></td>
 				</tr>
 				<tr>
 					<td><label>Havedage, åååå-mm-dd, adskilt af komma</label></td>
-					<td><textarea cols="30" rows="2" name="gardenday_dates"></textarea></td>
+					<td><textarea rows="2" name="gardenday_dates" style="width:100%;"><?php echo (intval(date('n')) < 4 ? date('Y') : strval(intval(date('Y') + 1))) . "-" . (intval(date('n')) >= 4 && intval(date('n')) <= 10 ? "10" : "04") . "-" . "01"; ?>,<?php echo (intval(date('n')) < 4 ? date('Y') : strval(intval(date('Y') + 1))) . "-" . (intval(date('n')) >= 4 && intval(date('n')) <= 10 ? "10" : "04") . "-" . "02"; ?>,<?php echo (intval(date('n')) < 4 ? date('Y') : strval(intval(date('Y') + 1))) . "-" . (intval(date('n')) >= 4 && intval(date('n')) <= 10 ? "10" : "04") . "-" . "03"; ?></textarea></td>
 				</tr>
 				<tr>
 					<td><label>Seneste tidspunkt for tilmelding</label></td>
-					<td><input type="datetime-local" name="latest_signup" /></td>
+					<td><input type="datetime-local" name="latest_signup" value="<?php echo (intval(date('n')) < 4 ? date('Y') : strval(intval(date('Y') + 1))) . "-" . (intval(date('n')) >= 4 && intval(date('n')) <= 10 ? "10" : "04") . "-" . "01" . "T" . "00" . ":" . "00"; ?>" style="width:100%;" /></td>
 				</tr>
 				<tr>
 					<td><label>Antal pladser pr. havedag</label></td>
-					<td><input type="number" name="spaces" /></td>
+					<td><input type="number" name="spaces" value="24" style="width:100%;" /></td>
 				</tr>
 				<tr>
 					<td><label>Antal pladser pr. lejlighed</label></td>
-					<td><input type="number" name="max_spaces" /></td>
+					<td><input type="number" name="max_spaces" value="1" style="width:100%;" /></td>
 				</tr>
 				<tr>
 					<td><label>Tidspunkt for offentliggørelse</label></td>
-					<td><input type="datetime-local" name="publish_date" /></td>
+					<td><input type="datetime-local" name="publish_date" value="<?php echo (intval(date('n')) < 4 ? date('Y') : strval(intval(date('Y') + 1))) . "-" . (intval(date('n')) >= 4 && intval(date('n')) <= 10 ? "10" : "04") . "-" . "01" . "T" . "00" . ":" . "00"; ?>" style="width:100%;" /></td>
 				</tr>
 				<tr>
 					<td></td>
