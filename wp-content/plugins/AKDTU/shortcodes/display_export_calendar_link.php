c828a6833db08e33634221eddd35a80373fa6abb
Victor Brandsen <vic.bra@hotmail.com>
2024-02-28 21:57:38 +0100

    Added formatting

--- wp-content/plugins/AKDTU/shortcodes/display_export_calendar_link.php
+++ wp-content/plugins/AKDTU/shortcodes/display_export_calendar_link.php
@@ -9,7 +9,7 @@ add_shortcode("AKDTU-display-calendar-export-link", "AKDTU_display_calendar_expo
  * @param array $atts Array of settings to be displayed
  * 
  * Default values:
- *   lang' => 'da': Language of the calendar
+ *   'lang' => 'da': Language of the calendar
  *   'before' => 'Du kan abonnere på kalenderen i Google Calendar, Outlook, mm. med følgende URL: ': Text to write before the html code-object
  *   'after' => '': Text to write after the html code-object
  * 
@@ -30,5 +30,5 @@ function AKDTU_display_calendar_export_link($atts) {
 	$user = wp_get_current_user();
 
 	# Return link to where the user can subscribe to the common house calendar, formatted in a html code-object
-	return $values['before'] . '<code>https://akdtu.dk/export-calendar.php?lang=' . $values['lang'] . '&user=' . $user->user_login . '&auth=' .  md5($user->user_pass) . '</code>' . $values['after'];
+	return $values['before'] . '<code style="user-select: all;-webkit-touch-callout: all;-webkit-user-select: all;-khtml-user-select: all;-moz-user-select: all;-ms-user-select: all;line-break: anywhere;">https://akdtu.dk/export-calendar.php?lang=' . $values['lang'] . '&user=' . $user->user_login . '&auth=' .  md5($user->user_pass) . '</code>' . $values['after'];
 }
