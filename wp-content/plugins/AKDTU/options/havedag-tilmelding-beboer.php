c34c3c20ddf32f085f7fdc103e91108b6a8c35d5
Victor Brandsen <vic.bra@hotmail.com>
2024-03-10 13:54:11 +0100

    Changed to use the most recent signup

--- wp-content/plugins/AKDTU/options/havedag-tilmelding-beboer.php
+++ wp-content/plugins/AKDTU/options/havedag-tilmelding-beboer.php
@@ -10,66 +10,65 @@ function AKDTU_havedag_tilmelding_beboer_mail_settings() {
 }
 
 function test_havedag_tilmelding_beboer_mail() {
-	$events = EM_Events::get(array('scope' => 'past', 'limit' => 1, 'offset' => 0, 'order' => 'ASC', 'orderby' => 'event_start', 'bookings' => true, 'owner' => false, 'pagination' => 0));
+	$events = EM_Events::get(array('limit' => 4, 'offset' => 0, 'order' => 'DESC', 'orderby' => 'event_start', 'bookings' => true, 'owner' => false, 'pagination' => 0));
+
+	$events = array_filter($events, function($EM_Event) {return count($EM_Event->get_bookings()->bookings) > 0;});
 
 	if (count($events) > 0) {
 		$EM_Event = $events[array_keys($events)[0]];
 
 		$bookings = $EM_Event->get_bookings()->bookings;
 
-		if (count($bookings) > 0) {
+		usort($bookings, function($a, $b) {return ((new DateTime($a->date()->format()))->getTimestamp() > (new DateTime($b->date()->format()))->getTimestamp() ? -1 : 1);});
 
-			$EM_Booking = $bookings[array_keys($bookings)[0]];
+		$EM_Booking = $bookings[array_keys($bookings)[0]];
 
-			$to_email = $EM_Booking->get_person()->data->user_email;
-			$from_email = get_option('dbem_event_submitted_email_admin');
+		$to_email = $EM_Booking->get_person()->data->user_email;
+		$from_email = get_option('dbem_event_submitted_email_admin');
 
-			echo '<h3>Mail ved tilmelding</h3><h4>Dansk:</h4>';
-			echo_AKDTU_email_as_table(
-				$to_email,
-				$from_email,
-				'',
-				'',
-				array(),
-				$EM_Booking->output(get_option('dbem_bookings_email_confirmed_subject_da'), 'raw'),
-				nl2br($EM_Booking->output(get_option('dbem_bookings_email_confirmed_body_da'), 'html'))
-			);
+		echo '<h3>Mail ved tilmelding</h3><h4>Dansk:</h4>';
+		echo_AKDTU_email_as_table(
+			$to_email,
+			$from_email,
+			'',
+			'',
+			array(),
+			$EM_Booking->output(get_option('dbem_bookings_email_confirmed_subject_da'), 'raw'),
+			nl2br($EM_Booking->output(get_option('dbem_bookings_email_confirmed_body_da'), 'html'))
+		);
 
-			echo '<h4>Engelsk:</h4>';
-			echo_AKDTU_email_as_table(
-				$to_email,
-				$from_email,
-				'',
-				'',
-				array(),
-				$EM_Booking->output(get_option('dbem_bookings_email_confirmed_subject_en'), 'raw'),
-				nl2br($EM_Booking->output(get_option('dbem_bookings_email_confirmed_body_en'), 'html'))
-			);
+		echo '<h4>Engelsk:</h4>';
+		echo_AKDTU_email_as_table(
+			$to_email,
+			$from_email,
+			'',
+			'',
+			array(),
+			$EM_Booking->output(get_option('dbem_bookings_email_confirmed_subject_en'), 'raw'),
+			nl2br($EM_Booking->output(get_option('dbem_bookings_email_confirmed_body_en'), 'html'))
+		);
 
-			echo '<br><hr><h3>Mail ved afmelding</h3><h4>Dansk:</h4>';
-			echo_AKDTU_email_as_table(
-				$to_email,
-				$from_email,
-				'',
-				'',
-				array(),
-				$EM_Booking->output(get_option('dbem_bookings_email_cancelled_subject_da'), 'raw'),
-				nl2br($EM_Booking->output(get_option('dbem_bookings_email_cancelled_body_da'), 'html'))
-			);
+		echo '<br><hr><h3>Mail ved afmelding</h3><h4>Dansk:</h4>';
+		echo_AKDTU_email_as_table(
+			$to_email,
+			$from_email,
+			'',
+			'',
+			array(),
+			$EM_Booking->output(get_option('dbem_bookings_email_cancelled_subject_da'), 'raw'),
+			nl2br($EM_Booking->output(get_option('dbem_bookings_email_cancelled_body_da'), 'html'))
+		);
 
-			echo '<h4>Engelsk:</h4>';
-			echo_AKDTU_email_as_table(
-				$to_email,
-				$from_email,
-				'',
-				'',
-				array(),
-				$EM_Booking->output(get_option('dbem_bookings_email_cancelled_subject_en'), 'raw'),
-				nl2br($EM_Booking->output(get_option('dbem_bookings_email_cancelled_body_en'), 'html'))
-			);
-		} else {
-			echo 'Der er ingen tilmeldinger på den seneste begivenhed der kan bruges til test...';
-		}
+		echo '<h4>Engelsk:</h4>';
+		echo_AKDTU_email_as_table(
+			$to_email,
+			$from_email,
+			'',
+			'',
+			array(),
+			$EM_Booking->output(get_option('dbem_bookings_email_cancelled_subject_en'), 'raw'),
+			nl2br($EM_Booking->output(get_option('dbem_bookings_email_cancelled_body_en'), 'html'))
+		);
 	} else {
 		echo 'Der er ingen begivenheder der kan bruges til test...';
 	}
--- wp-content/plugins/AKDTU/options/havedag-tilmelding-bestyrelse.php
+++ wp-content/plugins/AKDTU/options/havedag-tilmelding-bestyrelse.php
@@ -10,44 +10,43 @@ function AKDTU_havedag_tilmelding_bestyrelse_mail_settings() {
 };
 
 function test_havedag_tilmelding_bestyrelse_mail() {
-	$events = EM_Events::get(array('scope' => 'past', 'limit' => 1, 'offset' => 0, 'order' => 'ASC', 'orderby' => 'event_start', 'bookings' => true, 'owner' => false, 'pagination' => 0));
+	$events = EM_Events::get(array('limit' => 4, 'offset' => 0, 'order' => 'DESC', 'orderby' => 'event_start', 'bookings' => true, 'owner' => false, 'pagination' => 0));
+
+	$events = array_filter($events, function($EM_Event) {return count($EM_Event->get_bookings()->bookings) > 0;});
 
 	if (count($events) > 0) {
 		$EM_Event = $events[array_keys($events)[0]];
 
 		$bookings = $EM_Event->get_bookings()->bookings;
 
-		if (count($bookings) > 0) {
+		usort($bookings, function($a, $b) {return ((new DateTime($a->date()->format()))->getTimestamp() > (new DateTime($b->date()->format()))->getTimestamp() ? -1 : 1);});
 
-			$EM_Booking = $bookings[array_keys($bookings)[0]];
+		$EM_Booking = $bookings[array_keys($bookings)[0]];
 
-			$to_email = get_option('dbem_bookings_notify_admin');
-			$from_email = get_option('dbem_event_submitted_email_admin');
+		$to_email = get_option('dbem_bookings_notify_admin');
+		$from_email = get_option('dbem_event_submitted_email_admin');
 
-			echo '<h3>Mail ved tilmelding</h3>';
-			echo_AKDTU_email_as_table(
-				$to_email,
-				$from_email,
-				'',
-				'',
-				array(),
-				$EM_Booking->output(get_option('dbem_bookings_contact_email_confirmed_subject'), 'raw'),
-				nl2br($EM_Booking->output(get_option('dbem_bookings_contact_email_confirmed_body'), 'html'))
-			);
+		echo '<h3>Mail ved tilmelding</h3>';
+		echo_AKDTU_email_as_table(
+			$to_email,
+			$from_email,
+			'',
+			'',
+			array(),
+			$EM_Booking->output(get_option('dbem_bookings_contact_email_confirmed_subject'), 'raw'),
+			nl2br($EM_Booking->output(get_option('dbem_bookings_contact_email_confirmed_body'), 'html'))
+		);
 
-			echo '<br><hr><h3>Mail ved afmelding</h3>';
-			echo_AKDTU_email_as_table(
-				$to_email,
-				$from_email,
-				'',
-				'',
-				array(),
-				$EM_Booking->output(get_option('dbem_bookings_contact_email_cancelled_subject'), 'raw'),
-				nl2br($EM_Booking->output(get_option('dbem_bookings_contact_email_cancelled_body'), 'html'))
-			);
-		} else {
-			echo 'Der er ingen tilmeldinger på den seneste begivenhed der kan bruges til test...';
-		}
+		echo '<br><hr><h3>Mail ved afmelding</h3>';
+		echo_AKDTU_email_as_table(
+			$to_email,
+			$from_email,
+			'',
+			'',
+			array(),
+			$EM_Booking->output(get_option('dbem_bookings_contact_email_cancelled_subject'), 'raw'),
+			nl2br($EM_Booking->output(get_option('dbem_bookings_contact_email_cancelled_body'), 'html'))
+		);
 	} else {
 		echo 'Der er ingen begivenheder der kan bruges til test...';
 	}
