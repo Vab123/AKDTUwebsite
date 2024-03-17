dbcbb6cd3d476deb1b44588df3b2461b6b900bc4
Victor Brandsen <vic.bra@hotmail.com>
2024-03-17 12:55:25 +0100

    Updated formatting

--- wp-content/plugins/events-manager/classes/em-booking.php
+++ wp-content/plugins/events-manager/classes/em-booking.php
@@ -1137,7 +1137,7 @@ class EM_Booking extends EM_Object {
 
 					$date_formatter = new IntlDateFormatter("da_DK", IntlDateFormatter::LONG, IntlDateFormatter::NONE);
 
-					$replace = '<table>' . implode("",array_map(function($info,$date) use($date_formatter) {return '<tr><td>' . $date_formatter->format(new DateTime($date)) . '</td><td>' . $info['booked'] . ' / ' . $info['total'] . '</td></tr>';}, $spaces, array_keys($spaces))) . "<tr><td>" . 'Total</td><td>' . array_sum(array_map(function($info){return $info['booked'];}, $spaces)) . ' / ' . array_sum(array_map(function($info){return $info['total'];}, $spaces)) . '</td></tr></table>';
+					$replace = '<table>' . implode("",array_map(function($info,$date) use($date_formatter) {return '<tr><td>' . $date_formatter->format(new DateTime($date)) . '</td><td>' . $info['booked'] . '</td><td> / </td><td>' . $info['total'] . '</td></tr>';}, $spaces, array_keys($spaces))) . "<tr><td>" . 'Total</td><td>' . array_sum(array_map(function($info){return $info['booked'];}, $spaces)) . '</td><td> / </td><td>' . array_sum(array_map(function($info){return $info['total'];}, $spaces)) . '</td></tr></table>';
 					break;
 				case '#_BOOKINGID':
 					$replace = $this->booking_id;
