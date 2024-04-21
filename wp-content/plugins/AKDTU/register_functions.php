3dfe1d580956c7dacb2caa810a4d75d6991ab67b
Victor Brandsen <vic.bra@hotmail.com>
2024-04-21 18:24:20 +0200

    Added documentation

--- wp-content/plugins/AKDTU/register_functions.php
+++ wp-content/plugins/AKDTU/register_functions.php
@@ -18,34 +18,34 @@ $AKDTU_FUNCTIONS = array(
 
 $AKDTU_BOARD_TYPES = array(
 	'none' => array(
-		'id' => 0,
-		'name' => 'Beboer',
-		'user_level' => 'Beboer',
-		'user_role' => 'subscriber',
+		'id' => 0,													# ID of the user type, used in database
+		'name' => 'Beboer',											# Human readable name of the user type
+		'user_level' => 'Beboer',									# User level of the user type, as defined in the simple-membership plugin
+		'user_role' => 'subscriber',								# User role of the user type, as defined in wordpress (potentially through functions.php)
 	),
 	'default' => array(
-		'id' => 1,
-		'name' => 'Medlem',
-		'user_level' => 'Beboerprofil til bestyrelsesmedlem',
-		'user_role' => 'board_member',
+		'id' => 1,													# ID of the user type, used in database
+		'name' => 'Medlem',											# Human readable name of the user type
+		'user_level' => 'Beboerprofil til bestyrelsesmedlem',		# User level of the user type, as defined in the simple-membership plugin
+		'user_role' => 'board_member',								# User role of the user type, as defined in wordpress (potentially through functions.php)
 	),
 	'deputy' => array(
-		'id' => 2,
-		'name' => 'Suppleant',
-		'user_level' => 'Beboerprofil til bestyrelsessuppleant',
-		'user_role' => 'deputy',
+		'id' => 2,													# ID of the user type, used in database
+		'name' => 'Suppleant',										# Human readable name of the user type
+		'user_level' => 'Beboerprofil til bestyrelsessuppleant',	# User level of the user type, as defined in the simple-membership plugin
+		'user_role' => 'deputy',									# User role of the user type, as defined in wordpress (potentially through functions.php)
 	),
 	'chairman' => array(
-		'id' => 3,
-		'name' => 'Formand',
-		'user_level' => 'Beboerprofil til bestyrelsesmedlem',
-		'user_role' => 'board_member',
+		'id' => 3,													# ID of the user type, used in database
+		'name' => 'Formand',										# Human readable name of the user type
+		'user_level' => 'Beboerprofil til bestyrelsesmedlem',		# User level of the user type, as defined in the simple-membership plugin
+		'user_role' => 'board_member',								# User role of the user type, as defined in wordpress (potentially through functions.php)
 	),
 	'deputy-chairman' => array(
-		'id' => 4,
-		'name' => 'Næstformand',
-		'user_level' => 'Beboerprofil til bestyrelsesmedlem',
-		'user_role' => 'board_member',
+		'id' => 4,													# ID of the user type, used in database
+		'name' => 'Næstformand',									# Human readable name of the user type
+		'user_level' => 'Beboerprofil til bestyrelsesmedlem',		# User level of the user type, as defined in the simple-membership plugin
+		'user_role' => 'board_member',								# User role of the user type, as defined in wordpress (potentially through functions.php)
 	),
 );
 
