b7ba1d8dcaa855024653a4467282a042f8a4ec05
Victor Brandsen <vic.bra@hotmail.com>
2024-06-11 23:50:16 +0200

    Added option for more months

--- wp-content/plugins/AKDTU/widgets/fælleshus_vis_foreløbig_pris.php
+++ wp-content/plugins/AKDTU/widgets/fælleshus_vis_foreløbig_pris.php
@@ -5,49 +5,63 @@
  */
 
 function fælleshus_vis_foreløbig_pris_widget() {
-	$month_start = new DateTime("first day of this month", new DateTimeZone('Europe/Copenhagen'));
-	$month_end = new DateTime("last day of this month", new DateTimeZone('Europe/Copenhagen'));
+	$months = array(
+		// "last month",
+		"this month",
+		"next month",
+		// "+2 months"
+	);
 
-	$month = new IntlDateFormatter('da_DK', IntlDateFormatter::SHORT, IntlDateFormatter::SHORT, 'Europe/Copenhagen', null, 'MMMM');
+	$date = new IntlDateFormatter('da_DK', IntlDateFormatter::SHORT, IntlDateFormatter::SHORT, 'Europe/Copenhagen', null, 'MMMM YYYY');
 
-	$price_to_pay = get_price_to_pay($month_start, $month_end);
-	$price_adjustments = get_price_adjustments($month_start, $month_end);
-	$final_price = get_final_price($price_to_pay, $price_adjustments);
+	echo join("<br>", array_map(function ($month) use($date) {
+		$month_start = new DateTime("first day of " . $month, new DateTimeZone('Europe/Copenhagen'));
+		$month_end = new DateTime("last day of " . $month, new DateTimeZone('Europe/Copenhagen'));
 
-	if (count(array_keys($final_price)) > 0) :
-?>
-		<h3>Foreløbige opkrævninger for <?php echo $month->format($month_start); ?> måned:</h3>
-		<table class="widefat">
-			<colgroup>
-				<col span="1" style="width: 20%" />
-				<col span="1" style="width: 25%" />
-				<col span="1" style="width: 25%" />
-				<col span="1" style="width: 25%" />
-			</colgroup>
-			<thead>
-				<tr>
-					<th>Lejlighed</th>
-					<th>Pris for leje</th>
-					<th>Rettelser</th>
-					<th>Endelig pris</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php $row = 0;
-				foreach ($final_price as $username => $price) : ?>
-					<tr <?php if ($row % 2 == 0) {
-							echo 'class="alternate"';
-						};
-						$row++; ?>>
-						<td><?php echo padded_apartment_number_from_username($username) . (is_archive_user_from_username($username) ? ' (TB)' : ''); ?></td>
-						<td><?php echo number_format($price_to_pay[$username], 2, ",", "."); ?> kr.</td>
-						<td><?php echo number_format((isset($price_adjustments[apartment_number_from_username($username)]) ? $price_adjustments[apartment_number_from_username($username)] : 0), 2, ",", "."); ?> kr.</td>
-						<td><?php echo number_format($price, 2, ",", "."); ?> kr.</td>
-					</tr>
-				<?php endforeach; ?>
-			</tbody>
-		</table>
-	<?php else : ?>
-		Der er indtil videre ingen udlejninger for <?php echo $month->format($month_start); ?> måned.
-<?php endif;
+		$price_to_pay = get_price_to_pay($month_start, $month_end);
+		$price_adjustments = get_price_adjustments($month_start, $month_end);
+		$final_price = get_final_price($price_to_pay, $price_adjustments);
+
+		$return_string = "";
+
+		if (count(array_keys($final_price)) > 0) {
+			$return_string .= '<b>' . $date->format($month_start) . ':</b>';
+			$return_string .= '<table class="widefat">';
+			$return_string .= '<colgroup>';
+				$return_string .= '<col span="1" style="width: 20%" />';
+				$return_string .= '<col span="1" style="width: 25%" />';
+				$return_string .= '<col span="1" style="width: 25%" />';
+				$return_string .= '<col span="1" style="width: 25%" />';
+			$return_string .= '</colgroup>';
+			$return_string .= '<thead>';
+				$return_string .= '<tr>';
+					$return_string .= '<th>Lejlighed</th>';
+					$return_string .= '<th>Pris for leje</th>';
+					$return_string .= '<th>Rettelser</th>';
+					$return_string .= '<th>Endelig pris</th>';
+				$return_string .= '</tr>';
+			$return_string .= '</thead>';
+			$return_string .= '<tbody>';
+			
+			$row = 0;
+			foreach ($final_price as $username => $price) {
+				$return_string .= '<tr' . ($row % 2 == 0 ? ' class="alternate"' : '') . '>';
+				$row++;
+
+					$return_string .= '<td>' . padded_apartment_number_from_username($username) . (is_archive_user_from_username($username) ? ' (TB)' : '') . '</td>';
+					$return_string .= '<td>' . number_format($price_to_pay[$username], 2, ",", ".") . ' kr.</td>';
+					$return_string .= '<td>' . number_format((isset($price_adjustments[apartment_number_from_username($username)]) ? $price_adjustments[apartment_number_from_username($username)] : 0), 2, ",", ".") . ' kr.</td>';
+					$return_string .= '<td>' . number_format($price, 2, ",", ".") . ' kr.</td>';
+				$return_string .= '</tr>';
+			}
+			$return_string .= '</tbody>';
+		$return_string .= '</table>';
+		}
+		else {
+			$return_string = "Der er indtil videre ingen udlejninger for " . $month->format($month_start) . " måned.";
+		}
+
+		return $return_string;
+	}, $months));
+
 } ?>
