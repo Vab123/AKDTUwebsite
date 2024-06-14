7f562476386141ced32741bb80314c2b68100ce2
Victor Brandsen <vic.bra@hotmail.com>
2024-06-14 03:42:46 +0200

    Added version number

--- wp-content/plugins/AKDTU/shortcodes/display_export_calendar_link.php
+++ wp-content/plugins/AKDTU/shortcodes/display_export_calendar_link.php
@@ -12,6 +12,7 @@ add_shortcode("AKDTU-display-calendar-export-link", "AKDTU_display_calendar_expo
  *   'lang' => 'da': Language of the calendar
  *   'before' => 'Du kan abonnere på kalenderen i Google Calendar, Outlook, mm. med følgende URL: ': Text to write before the html code-object
  *   'after' => '': Text to write after the html code-object
+ *	 'version' => '1.0': Version text
  * 
  * @return string Link to where the user can subscribe to the common house calendar, formatted in a html code-object
  */
@@ -21,6 +22,7 @@ function AKDTU_display_calendar_export_link($atts) {
 		'lang' 				=> 'da', 																				# Language of the calendar
 		'before' 			=> 'Du kan abonnere på kalenderen i Google Calendar, Outlook, mm. med følgende URL: ',	# Text to write before the html code-object
 		'after' 			=> '', 																					# Text to write after the html code-object
+		'version' 			=> '1.0', 																				# Version text
 	);
 	
 	# Combine default values and provided settings
@@ -30,5 +32,5 @@ function AKDTU_display_calendar_export_link($atts) {
 	$user = wp_get_current_user();
 
 	# Return link to where the user can subscribe to the common house calendar, formatted in a html code-object
-	return $values['before'] . '<code style="user-select: all;-webkit-touch-callout: all;-webkit-user-select: all;-khtml-user-select: all;-moz-user-select: all;-ms-user-select: all;line-break: anywhere;">https://akdtu.dk/export-calendar.php?lang=' . $values['lang'] . '&user=' . $user->user_login . '&auth=' .  md5($user->user_pass) . '</code>' . $values['after'];
+	return $values['before'] . '<code style="user-select: all;-webkit-touch-callout: all;-webkit-user-select: all;-khtml-user-select: all;-moz-user-select: all;-ms-user-select: all;line-break: anywhere;">https://akdtu.dk/export-calendar.php?lang=' . $values['lang'] . '&user=' . $user->user_login . '&auth=' .  md5($user->user_pass) . '&vers=' . $values['version'] . '</code>' . $values['after'];
 }
