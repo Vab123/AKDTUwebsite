fbdc11f86f7ecec0360df922a28c2a3d8ef11535
Victor Brandsen <vic.bra@hotmail.com>
2024-07-09 00:36:29 +0200

    Moved common house booking functions to separate file

--- wp-content/plugins/AKDTU/actions/book_fælleshus.php
+++ wp-content/plugins/AKDTU/actions/book_fælleshus.php
@@ -29,63 +29,30 @@ if (isset($_REQUEST['action'])) {
  * @return bool True if the booking was successfully created
  */
 function book_fælleshus_beboer($event_owner, $title, $start_date, $end_date) {
-	global $wpdb;
-
 	# Check if end-date is before or the same as the start-date
 	if ($end_date <= $start_date) {
 		new AKDTU_notice('error','Sluttidspunkt skal være før starttidspunkt.');
 		return false;
 	}
 
-	# Create Wordpress post
-	$id = wp_insert_post( array('post_status' => 'publish', 'post_type' => 'event', 'post_title' => $title,'post_content' => '', 'post_date' => current_time("Y-m-d H:i:s"), 'post_author' => $event_owner->ID ) );
+	$params = array(
+		'title' => $title,
+		'event_owner_id' => $event_owner->ID,
+		'start_date' => $start_date,
+		'end_date' => $end_date,
+		'all_day' => false,
+		'event_language' => 'da_DK',
+	);
 
-	# Protect event
-	SwpmProtection::get_instance()->apply(array($id), 'custom_post')->save();
-	$query = "SELECT id FROM " . $wpdb->prefix . "swpm_membership_tbl WHERE id !=1 ";
-	$level_ids = $wpdb->get_col($query);
-	foreach ($level_ids as $level) {
-		SwpmPermission::get_instance($level)->apply(array($id), 'custom_post')->save();
-	}
+	$new_event_id = book_common_house($params);
 
-	# Set post language
-	pll_set_post_language($id,'da_DK');
+	if (is_numeric($new_event_id)) {
+		new AKDTU_notice('success', 'Fælleshuset er nu reserveret.');
 
-	# Set event details
-	$event = new EM_Event($id,'post_id');
-
-	$event->__set('event_start_date', $start_date->format("Y-m-d")); # Start date of event
-	$event->__set('event_end_date', $end_date->format("Y-m-d")); # End date of event
-	$event->__set('event_start_time', $start_date->format("H:i:s")); # Start time of event
-	$event->__set('event_end_time', $end_date->format("H:i:s")); # End time of event
-	$event->__set('event_start', $start_date->format("Y-m-d") . ' ' . $start_date->format("H:i:s")); # Start of event
-	$event->__set('event_end', $end_date->format("Y-m-d") . ' ' . $end_date->format("H:i:s")); # End of event
-	$event->__set('start_date', $start_date->format("Y-m-d")); # Start date of event
-	$event->__set('end_date', $end_date->format("Y-m-d")); # End date of event
-	$event->__set('start_time', $start_date->format("H:i:s")); # Start time of event
-	$event->__set('end_time', $end_date->format("H:i:s")); # End time of event
-
-	$event->__set('location_id', 0); # Location ID
-	$event->__set('event_spaces', NULL); # Total amount of spaces on event
-	$event->__set('event_private', 0); # Boolean, is event private
-	$event->__set('event_language', 'da_DK'); # Language of event
-	$event->__set('blog_id',get_current_blog_id()); # ID of blog, for multi-site installations
-	$event->__set('event_owner', $event_owner->ID); # Language of event
-	$event->set_status(1, true); # Publish event
-
-	update_post_meta($event->post_id, '_event_approvals_count', 1); # Set approval status
-
-	if ($event->validate() && $event->save_meta() && $event->save()){ # Save event and event metadata
-		# Saving was successful. Write success message to the admin interface
-		new AKDTU_notice('success','Fælleshuset er nu reserveret.');
-		
 		return true;
 	} else {
 		# Saving failed. Write error message to the admin interface
-		new AKDTU_notice('error','Fælleshuset kunne ikke reserveres.');
-		foreach ($event->errors as $error) {
-			new AKDTU_notice('warning',$error);
-		}
+		new AKDTU_notice('error', 'Fælleshuset kunne ikke reserveres.');
 
 		return false;
 	}
@@ -104,94 +71,40 @@ function book_fælleshus_beboer($event_owner, $title, $start_date, $end_date) {
  * @return bool True if the booking was successfully created
  */
 function book_fælleshus_bestyrelse($event_owner, $title_da, $title_en, $start_date, $end_date, $all_day) {
-	global $wpdb;
-
 	if ($end_date <= $start_date) {
 		new AKDTU_notice('error','Sluttidspunkt skal være før starttidspunkt.');
 		return false;
 	}
 
-	# Create Wordpress posts
-	$id_da = wp_insert_post( array('post_status' => 'publish', 'post_type' => 'event', 'post_title' => $title_da,'post_content' => '', 'post_date' => current_time("Y-m-d H:i:s"), 'post_author' => $event_owner->ID ) );
-	$id_en = wp_insert_post( array('post_status' => 'publish', 'post_type' => 'event', 'post_title' => $title_en,'post_content' => '', 'post_date' => current_time("Y-m-d H:i:s"), 'post_author' => $event_owner->ID ) );
+	$params_da = array(
+		'title' => $title_da,
+		'event_owner_id' => $event_owner->ID,
+		'start_date' => $start_date,
+		'end_date' => $end_date,
+		'all_day' => $all_day,
+		'event_language' => 'da_DK',
+	);
+	
+	$event_da_id = book_common_house($params_da);
 
-	# Protect event
-	SwpmProtection::get_instance()->apply(array($id_da), 'custom_post')->save();
-	SwpmProtection::get_instance()->apply(array($id_en), 'custom_post')->save();
-	$level_ids = $wpdb->get_col("SELECT id FROM " . $wpdb->prefix . "swpm_membership_tbl WHERE id !=1");
-	foreach ($level_ids as $level) {
-		SwpmPermission::get_instance($level)->apply(array($id_da), 'custom_post')->save();
-		SwpmPermission::get_instance($level)->apply(array($id_en), 'custom_post')->save();
-	}
+	$params_en = array(
+		'title' => $title_en,
+		'event_owner_id' => $event_owner->ID,
+		'start_date' => $start_date,
+		'end_date' => $end_date,
+		'all_day' => $all_day,
+		'event_language' => 'en_US',
+	);
+	
+	$event_en_id = book_common_house($params_en);
 
-	# Set post language
-	pll_set_post_language($id_da,'da_DK');
-	pll_set_post_language($id_en,'en_US');
-
-	# Set Danish event details
-	$event_da = new EM_Event($id_da,'post_id');
-
-	$event_da->__set('event_start_date', $start_date->format("Y-m-d")); # Start date of event
-	$event_da->__set('event_end_date', $end_date->format("Y-m-d")); # End date of event
-	$event_da->__set('event_start_time', ($all_day ? "00:00:00" : $start_date->format("H:i:s"))); # Start time of event
-	$event_da->__set('event_end_time', ($all_day ? "23:59:59" : $end_date->format("H:i:s"))); # End time of event
-	$event_da->__set('event_start', $start_date->format("Y-m-d") . ' ' . $start_date->format("H:i:s")); # Start of event
-	$event_da->__set('event_end', $end_date->format("Y-m-d") . ' ' . $end_date->format("H:i:s")); # End of event
-	$event_da->__set('start_date', $start_date->format("Y-m-d")); # Start date of event
-	$event_da->__set('end_date', $end_date->format("Y-m-d")); # End date of event
-	$event_da->__set('start_time', ($all_day ? "00:00:00" : $start_date->format("H:i:s"))); # Start time of event
-	$event_da->__set('end_time', ($all_day ? "23:59:59" : $end_date->format("H:i:s"))); # End time of event
-	$event_da->__set('event_all_day', $all_day); # If event is an all-day event
-
-	$event_da->__set('location_id', 0); # Location ID
-	$event_da->__set('event_spaces', NULL); # Total amount of spaces on event
-	$event_da->__set('event_private', 0); # Boolean, is event private
-	$event_da->__set('event_language', 'da_DK'); # Language of event
-	$event_da->__set('blog_id',get_current_blog_id()); # ID of blog, for multi-site installations
-	$event_da->__set('event_owner', $event_owner->ID); # Language of event
-	$event_da->set_status(1, true); # Publish event
-
-	update_post_meta($event_da->post_id, '_event_approvals_count', 1); # Set approval status
-
-	# Set English event details
-	$event_en = new EM_Event($id_en,'post_id');
-
-	$event_en->__set('event_start_date', $start_date->format("Y-m-d")); # Start date of event
-	$event_en->__set('event_end_date', $end_date->format("Y-m-d")); # End date of event
-	$event_en->__set('event_start_time', ($all_day ? "00:00:00" : $start_date->format("H:i:s"))); # Start time of event
-	$event_en->__set('event_end_time', ($all_day ? "23:59:59" : $end_date->format("H:i:s"))); # End time of event
-	$event_en->__set('event_start', $start_date->format("Y-m-d") . ' ' . $start_date->format("H:i:s")); # Start of event
-	$event_en->__set('event_end', $end_date->format("Y-m-d") . ' ' . $end_date->format("H:i:s")); # End of event
-	$event_en->__set('start_date', $start_date->format("Y-m-d")); # Start date of event
-	$event_en->__set('end_date', $end_date->format("Y-m-d")); # End date of event
-	$event_en->__set('start_time', ($all_day ? "00:00:00" : $start_date->format("H:i:s"))); # Start time of event
-	$event_en->__set('end_time', ($all_day ? "23:59:59" : $end_date->format("H:i:s"))); # End time of event
-	$event_en->__set('event_all_day', $all_day); # If event is an all-day event
-
-	$event_en->__set('location_id', 0); # Location ID
-	$event_en->__set('event_spaces', NULL); # Total amount of spaces on event
-	$event_en->__set('event_private', 0); # Boolean, is event private
-	$event_en->__set('event_language', 'en_US'); # Language of event
-	$event_en->__set('blog_id',get_current_blog_id()); # ID of blog, for multi-site installations
-	$event_en->__set('event_owner', $event_owner->ID); # Language of event
-	$event_en->set_status(1, true); # Publish event
-
-	update_post_meta($event_en->post_id, '_event_approvals_count', 1); # Set approval status
-
-	if ($event_da->validate() && $event_da->save_meta() && $event_da->save() && $event_en->validate() && $event_en->save_meta() && $event_en->save()){ # Save event and event metadata
-		pll_save_post_translations( array('en' => $id_en, 'da' => $id_da ) );
-		new AKDTU_notice('success','Fælleshuset er nu reserveret.');
+	if (is_numeric($event_da_id) && is_numeric($event_en_id)){ # Save event and event metadata
+		pll_save_post_translations( array('en' => $event_en_id, 'da' => $event_da_id ) );
+		new AKDTU_notice('success', 'Fælleshuset er nu reserveret.');
 
 		return true;
 	} else {
-		new AKDTU_notice('error','Fælleshuset kunne ikke reserveres.');
-
-		foreach ($event_da->errors as $error) {
-			new AKDTU_notice('warning','DA: ' . $error);
-		}
-		foreach ($event_en->errors as $error) {
-			new AKDTU_notice('warning','EN: ' . $error);
-		}
+		new AKDTU_notice('error', 'Fælleshuset kunne ikke reserveres. Dansk begivenhed blev ' . (is_numeric($event_da_id) ? '' : 'ikke ') . 'oprettet. Engelsk begivenhed blev ' . (is_numeric($event_en_id) ? '' : 'ikke ') . 'oprettet.');
 		
 		return false;
 	}
