68b18bd37d824b7431f4c138693e795c3b1f561c
Victor Brandsen <vic.bra@hotmail.com>
2024-07-13 17:50:00 +0200

    Added current password to widget

--- wp-content/plugins/AKDTU/widgets/fælleshus_internet_set.php
+++ wp-content/plugins/AKDTU/widgets/fælleshus_internet_set.php
@@ -13,8 +13,8 @@ function fælleshus_internet_set_dashboard_widget() {
 	$router_settings = get_router_settings(); ?>
 		<table width="100%" class="widefat">
 			<colgroup>
-				<col span="1" style="width: 40%">
 				<col span="1" style="width: 60%">
+				<col span="1" style="width: 40%">
 			</colgroup>
 			<thead>
 				<tr>
@@ -35,13 +35,21 @@ function fælleshus_internet_set_dashboard_widget() {
 					</tr>
 					<tr>
 						<td style="vertical-align:middle">
-							Adgangskode
+							Nuværende adgangskode
+						</td>
+						<td style="vertical-align:middle">
+							<b><?php echo get_fælleshus_password()['wl0_wpa_psk']; ?></b>
+						</td>
+					</tr>
+					<tr class="alternate">
+						<td style="vertical-align:middle">
+							Ny adgangskode
 						</td>
 						<td style="vertical-align:middle">
 							<input type="text" name="new_password" placeholder="Ny adgangskode" />
 						</td>
 					</tr>
-					<tr class="alternate">
+					<tr>
 						<td style="vertical-align:middle">
 							Fælleshus status
 						</td>
@@ -49,7 +57,7 @@ function fælleshus_internet_set_dashboard_widget() {
 							<b><?php echo ($rented ? "Lejet af " . $event_owners : "Ledigt"); ?></b>
 						</td>
 					</tr>
-					<tr>
+					<tr class="alternate">
 						<td style="vertical-align:middle"></td>
 						<input type="hidden" name="action" value="fælleshus_internet_force_set" />
 						<td style="vertical-align:middle"><input type="submit" class="button-secondary" value="Sæt adgangskode" /></td>
